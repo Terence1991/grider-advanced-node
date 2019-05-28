1a3636f04006ccd5b9965bf5468840903a6e7b52
terence1991 <cplterencehawes@gmail.com>
2019-05-28 11:42:50 -0400

    cluster implementation ti server

--- index.js
+++ index.js
@@ -1,16 +1,23 @@
 const cluster = require('cluster');
 const express = require('express');
 
-console.log(cluster.isMaster)
 
-const app = express()
+//by running in comand line we are 
+//asking if the file is being executed 
+if(cluster.isMaster) {
+    //cause index.js to be exexuted again 
+    // in child mode 
+    cluster.fork()
+} else {
+    // Im a child I am going to act like a server
+    //and do nothing else 
+    const app = express()
 
-app.get('/', (req, res) => {
-
-    res.send('Hi there!')
-
-})
-
-
-
-app.listen(3000)
+    app.get('/', (req, res) => {
+    
+        res.send('Hi there!')
+    
+    })
+    
+    app.listen(3000)   
+}
